3682391d1a33cabcb6b5b5de8c4e459f0d5805c6
matias9809 <matiasloiza@hotmail.es>
2023-01-02 11:09:54 -0300

    ejercicio 2

---
+++ index.js
@@ -0,0 +1,22 @@
+/* 1- Generar una función que reciba como parámetro el array de cervezas y un valor de alcohol. 
+La función debe devolver un nuevo arreglo con las cervezas que no excedan el nivel etílico. 
+Cada elemento del nuevo arreglo debe ser un objeto que tenga el nombre propiedades, 
+alcohol (abv) y "amargor" (ibu)
+
+2- Generar una función que reciba como parámetro un array de cervezas y devuelva un nuevo 
+array con las 10 cervezas más alcohólicas
+
+3- Generar una función que reciba como parámetros un array de cervezas y devuelva 
+un nuevo array con las 10 cervezas menos amargas
+
+4- Generar una función que reciba como parámetro un array de cervezas, 
+un nombre de propiedad y un valor booleano.
+Debe devolver un nuevo arreglo con 10 cervezas ordenadas por la propiedad ingresada 
+como segundo argumento, de manera ascendente si el tercero es verdadero o 
+descendente si es falso
+
+5- Generar una función que reciba como parámetro un array de cervezas y un id.
+La función debe renderizar (renderear, dibujar, pintar, llenar, etc) en un archivo
+html una tabla que contenga las columnas "Name", "ABV", "IBU", y una fila por cada elemento del array.
+Cada fila debe tener los datos que se piden de cada una de las cervezas.
+*/
